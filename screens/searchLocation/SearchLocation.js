872860f43ba312fd30567fbdbebfc8b169e1662a
Waruni Lalendra <warunilalendra@gmail.com>
2024-02-02 12:55:25 +0530

    Search bar UI created

--- screens/searchLocation/SearchLocation.js
+++ screens/searchLocation/SearchLocation.js
@@ -1,13 +1,65 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet, TextInput, TouchableOpacity, Image} from "react-native";
 
-export default function SearchLocation(){
+export default function SearchLocation() {
 
 
     return (
         <View>
-            <Text>
-                this is search location
-            </Text>
+            <View style={styles.searchContainer}>
+                <View style={styles.searchWrapper}>
+                    <TextInput
+                        style={styles.searchInput}
+                        placeholder='Enter the place you want to locate'
+                    />
+                </View>
+
+                <TouchableOpacity style={styles.searchBtn}>
+                    <Image
+                        source={require("../../assets/images/search-location.png")}
+                        resizeMode='contain'
+                        style={styles.searchBtnImage}
+                    />
+                </TouchableOpacity>
+            </View>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    searchContainer: {
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "row",
+        marginTop: 35,
+        height: 50,
+        marginHorizontal: 10
+      },
+      searchWrapper: {
+        flex: 1,
+        backgroundColor: "white",
+        marginRight: 12,
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: 16,
+        height: "100%",
+      },
+      searchInput: {
+        fontFamily: "Roboto",
+        width: "100%",
+        height: "100%",
+        paddingHorizontal: 16,
+      },
+      searchBtn: {
+        width: 50,
+        height: "100%",
+        backgroundColor: "#84E9BD",
+        borderRadius: 16,
+        justifyContent: "center",
+        alignItems: "center",
+      },
+      searchBtnImage: {
+        width: "50%",
+        height: "50%",
+        tintColor: "white",
+      }
+})
