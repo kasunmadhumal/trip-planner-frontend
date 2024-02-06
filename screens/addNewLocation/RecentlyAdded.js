0740bc5c201ea7f70f397a3f5e90bfb36976e55d
Waruni Lalendra <warunilalendra@gmail.com>
2024-02-06 15:58:53 +0530

    UI structure for 3 of add-new-location screens created

--- screens/addNewLocation/RecentlyAdded.js
+++ screens/addNewLocation/RecentlyAdded.js
@@ -1,4 +1,4 @@
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View, ScrollView } from "react-native";
 import FormChangeButton from "../../components/FormChangeButton";
 import FormSubmitButton from "../../components/FormSubmitButton";
 
@@ -6,21 +6,27 @@ import FormSubmitButton from "../../components/FormSubmitButton";
 export default function RecentlyAdded({ pageNumber, setPageNumber }) {
   return (
     <View style={styles.container}>
-      <Text>Page 3</Text>
-      <View style={styles.buttonsContainer}>
-        <FormChangeButton
-          text="Preview"
-          onPress={() => {
-            setPageNumber(pageNumber - 1);
-          }}
-        />
-        <FormSubmitButton
-          text="Next"
-          onPress={() => {
-            setPageNumber(pageNumber + 1);
-          }}
-        />
-      </View>
+      <ScrollView style={styles.scrollView}>
+        <Text style={styles.tittleText}>Recently Added</Text>
+        <View style={styles.formContainer}></View>
+        <Text style={styles.tittleText}>Added Places</Text>
+        <View style={styles.formContainer}>
+          <View style={styles.buttonsContainer}>
+            <FormChangeButton
+              text="Preview"
+              onPress={() => {
+                setPageNumber(pageNumber - 1);
+              }}
+            />
+            <FormChangeButton
+              text="Next"
+              onPress={() => {
+                setPageNumber(pageNumber + 1);
+              }}
+            />
+          </View>
+        </View>
+      </ScrollView>
     </View>
   );
 }
@@ -38,5 +44,22 @@ const styles = StyleSheet.create({
     marginTop: "3%",
     flexDirection: "row",
     justifyContent: "space-between",
+  },
+  tittleText: {
+    fontSize: 20,
+    fontWeight: "bold",
+    fontFamily: "Roboto",
+    color: "#2F264B",
+    alignSelf: "flex-start",
+    marginLeft: "10%",
+    marginBottom: "3%",
+  },
+  scrollView: {
+    width: "100%",
+  },
+  formContainer: {
+    alignItems: "center",
+    justifyContent: "flex-start",
+    width: "100%",
   },
 });
